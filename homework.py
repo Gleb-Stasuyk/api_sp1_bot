727d813e391189439493851a7361f89d4556c4a7
Gleb-Stasuyk <gleb.stasuyk@gmail.com>
2020-10-08 17:14:07 +0300

    last

--- homework.py
+++ homework.py
@@ -1,10 +1,10 @@
 import logging
 import os
 import requests
+import sys
 import time
 
 from dotenv import load_dotenv
-from inspect import currentframe
 import telegram
 
 load_dotenv()
@@ -15,6 +15,10 @@ TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 bot = telegram.Bot(token=TELEGRAM_TOKEN)
 logger = logging.getLogger(__name__)
+path_to_logger = sys.argv[0]
+path_to_logger_dir = os.path.dirname(os.path.abspath(path_to_logger))
+name = 'Log.log'
+filename = os.path.join(path_to_logger_dir, name)
 
 
 def parse_homework_status(homework):
@@ -22,8 +26,8 @@ def parse_homework_status(homework):
     if homework['status'] != 'approved':
         verdict = 'К сожалению в работе нашлись ошибки.'
     else:
-        verdict = 'Ревьюеру всё понравилось, ' + \
-                  'можно приступать к следующему уроку.'
+        verdict = ('Ревьюеру всё понравилось, '
+                   'можно приступать к следующему уроку.')
     return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
 
 
@@ -37,16 +41,19 @@ def get_homework_statuses(current_timestamp):
             params=date
         ).json()
         if homework_statuses.get('error'):
-            raise RuntimeError("Homework status contains error: "
-                               + homework_statuses['error']['error']
-                               + f'line:{currentframe().f_lineno}')
+            error = (f'Homework status contains error: '
+                     f'{homework_statuses["error"]["error"]}')
+            logger.error(error)
+            raise RuntimeError(error)
         if homework_statuses.get('code') == 'not_authenticated':
-            raise RuntimeError(f'Authorisation Error. '
-                               f'line:{currentframe().f_lineno}')
+            error = f'Authorisation Error. not_authenticated'
+            logger.error(error)
+            raise RuntimeError(error)
         return homework_statuses
     except (requests.exceptions.RequestException, ValueError) as e:
-        raise RuntimeError(f'Request homework status failed with error:{e}. '
-                           f'line:{currentframe().f_lineno}')
+        error = f'Request homework status failed with error:{e}.'
+        logger.error(error)
+        raise RuntimeError(error)
 
 
 def send_message(message):
@@ -55,24 +62,23 @@ def send_message(message):
 
 def main():
     current_timestamp = int(time.time())
-    send_message('Bot launch')
+
     while True:
         try:
             new_homework = get_homework_statuses(current_timestamp)
             if new_homework.get('homeworks'):
-                send_message(parse_homework_status(
-                    new_homework['homeworks'][0]))
-            current_timestamp = new_homework.get('current_date',
-                                                 int(time.time()))
+                send_message(parse_homework_status(new_homework.get('homeworks')[0]))
+            current_timestamp = new_homework.get('current_date')
             time.sleep(1200)
 
         except Exception as e:
-            logger.error(f'Unsuccessful bot launching with error: {e}')
-            time.sleep(50)
+            print(f'Бот упал с ошибкой: {e}')
+            time.sleep(5)
+            continue
 
 
 if __name__ == '__main__':
-    logging.basicConfig(filename="Log.log", level=logging.ERROR,
+    logging.basicConfig(filename=filename, level=logging.ERROR,
                         format=u'%(filename)s[LINE:%(lineno)d]# '
                                u'%(levelname)-8s [%(asctime)s]  %(message)s')
     main()
